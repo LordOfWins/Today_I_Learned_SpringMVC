b8a1d1b334e83c231218aa35c661b2eeb205b525
윤승재 <sjyun011278@gmail.com>
2022-10-14 17:28:33 +0900

    ApplicationScope

--- ApplicationScope/src/main/java/kr/co/ezenac/config/SpringConfigClass.java
+++ ApplicationScope/src/main/java/kr/co/ezenac/config/SpringConfigClass.java
@@ -1,6 +1,5 @@
 package kr.co.ezenac.config;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -11,7 +10,6 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpringConfigClass implements WebApplicationInitializer {
 
