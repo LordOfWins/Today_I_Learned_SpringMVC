7c3d90d0187ad9fc2babfbb2c900343617c05e53
윤승재 <sjyun011278@gmail.com>
2022-10-21 14:58:12 +0900

    MyBatisJava

--- MyBatisJava/src/main/java/kr/co/softcampus/database/MapperInterface.java
+++ MyBatisJava/src/main/java/kr/co/softcampus/database/MapperInterface.java
@@ -1,6 +1,9 @@
 package kr.co.softcampus.database;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import kr.co.softcampus.beans.DataBean;
 
@@ -8,4 +11,9 @@ public interface MapperInterface {
 
 	@Insert("insert into spring_mvc_table(data1, data2, data3) values (#{data1}, #{data2}, #{data3})")
 	void insert_data(DataBean dataBean);
+
+	@Select("select data1, data2, data3 from spring_mvc_table")
+	// 반환 타입이 2개 이상
+	List<DataBean> select_data();
+
 }
